04833da7a32e69e5d1df31391815004f45feb498
kirega <joseph.mutiga@gmail.com>
2023-01-11 16:17:37 +0300

    first crack at drag drop api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function handleDragStart(ev: any) {
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+  }
+
+  const handleDragEnd = (ev: any) => {
+    ev.preventDefault();
+  };
+  const handleDragEnterForTarget = (ev: any) => {
+    ev.preventDefault();
+  };
+  const handleDragOverForTarget = (ev: any) => {
+    ev.preventDefault();
+  };
+  const handleOnDrop = (ev: any) => {
+    const id = ev.dataTransfer.getData("text/plain");
+    const child = document.getElementById(id);
+    const parent = document.getElementById(id)?.parentNode;
+    if (parent === ev.target) {
+      return false;
+    } else {
+      ev.target.appendChild(child);
+    }
+  };
+  const items = [
+    {
+      id: crypto.randomUUID(),
+      name: "Hello",
+      desc: "World",
+    },
+    {  
+      id: crypto.randomUUID(),
+      name: "Out",
+      desc: "In the outdoors",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "Mobile",
+      desc: "technology",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="columns">
+      <div
+        id="left"
+        onDragEnter={handleDragEnterForTarget}
+        onDragOver={handleDragOverForTarget}
+        onDrop={handleOnDrop}
+      >
+        {items.map((item, key) => (
+          <div
+            id={item.id}
+            key={key}
+            className="child"
+            draggable="true"
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          >
+            <p>{item.name}</p>
+            <br/>
+            <p>{item.desc}</p>
+          </div>
+        ))}
+      </div>
+      <div
+        id="right"
+        onDragEnter={handleDragEnterForTarget}
+        onDragOver={handleDragOverForTarget}
+        onDrop={handleOnDrop}
+      ></div>
     </div>
   );
 }
